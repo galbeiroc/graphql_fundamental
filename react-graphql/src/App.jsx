b5d146306bd12f7ede0b52445de529094437e178
Albeiro Crespo <alcresgu_1989@hotmail.com>
2023-03-24 17:44:44 -0500

    using graphql with fetch

--- react-graphql/src/App.jsx
+++ react-graphql/src/App.jsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:4000/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: `
+        query {
+          allPersons {
+            name
+          }
+        }
+      `})
+    })
+    .then((res) => res.json())
+    .then((res) => console.log(res.data))
+  }, [])
+
   return (
     <div className="App">
       <div>
