38dd3d32e2fa817dd41aaa776723d30ed9dbd00e
Albeiro Crespo <alcresgu_1989@hotmail.com>
2023-03-24 17:28:02 -0500

    set react app

---
+++ react-graphql/src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React + GraphQL!</h1>
+    </div>
+  )
+}
+
+export default App
