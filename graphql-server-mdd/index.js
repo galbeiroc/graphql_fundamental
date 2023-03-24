af0448c2deb11f3b4452dc5155fdaa24ac8f6e9b
Albeiro Crespo <alcresgu_1989@hotmail.com>
2023-03-24 16:10:40 -0500

    implement definitions and query

---
+++ graphql-server-mdd/index.js
@@ -0,0 +1,60 @@
+import { gql, ApolloServer } from 'apollo-server';
+
+const persons = [
+  {
+    name: 'galbeiroc',
+    phone: '301-545889',
+    street: 'Calle Florida',
+    city: 'La Estrella',
+    id: '3fa85f64-5717-4562-b3fc-2c963f66afa6'
+  },
+  {
+    name: 'leo',
+    street: 'Av Colombia',
+    city: 'Robledo',
+    id: '3fa85f64-5717-4562-b3fc-2c963f66054a'
+  },
+  {
+    name: 'karen',
+    phone: '321-145872',
+    street: 'Principal Comfama',
+    city: 'Sabaneta',
+    id: '3fa85f64-5717-4562-b3fc-2c963f66cca21'
+  }
+];
+
+// Definitions
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person]!
+    findPerson(name: String!): Person
+  }
+`
+
+// Resolvers
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => {
+      const { name } = args;
+      return persons.find((person) => person.name === name);
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => console.log(`Server ready at ${url}`));
